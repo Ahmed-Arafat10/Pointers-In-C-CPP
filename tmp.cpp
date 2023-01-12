262bb8f727f4f321235f46f820881e1da62953ec
Ahmed_Arafat10 <ahmedxarafat0101@gmail.com>
2023-01-12 04:52:53 +0200

    4:52am 1/12/2023

--- tmp.cpp
+++ tmp.cpp
@@ -1,15 +1,20 @@
 // Created by Ahmed Arafat on 1/10/2023.
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int a = 1025;
-    int *p;
-    p = &a;
-    printf("Address of a %d\n", &a); //200
-    printf("Address of p : %d , value of p: %d \n",p+1,*(p+1)); // 204 garbage
-    char *p0;
-    p0 = (char *)p;
-    printf("Size of character %d\n", sizeof(char)); // 1
-    printf("Address of c : %d , value of c: %d \n",p0+1,*(p0+1)); // 201 4
+void Double(int arr[], int sz) {
+    int SZ = sizeof(arr)/sizeof(arr[0]); // 2
+    printf("Size of arr %d & Size of arr[0] %d \n",sizeof(arr),sizeof(arr[0]));// 8 4
+    for (int i = 0; i < sz; i++) {
+        *(arr + i) = *(arr + i) * 2;
+        // Or
+        // arr[i] = arr[i] * 2;
+    }
+}
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5}; //Address 200 204 208 212 216
+    int sz = sizeof(arr) / sizeof(arr[0]);
+    Double(arr, sz);
+    for (int i = 0; i < sz; i++) {
+        printf("%d\n", *(arr + i));
+    }
 }
