8aeecec60698ff8e25a7954b3084167a96a49c4a
Ahmed_Arafat10 <ahmedxarafat0101@gmail.com>
2023-01-26 12:08:56 +0200

    12:08 pm 1/26/23

--- tmp.cpp
+++ tmp.cpp
@@ -1,20 +1,19 @@
 // Created by Ahmed Arafat on 1/10/2023.
 #include <bits/stdc++.h>
 using namespace std;
-void Double(int arr[], int sz) {
-    int SZ = sizeof(arr)/sizeof(arr[0]); // 2
-    printf("Size of arr %d & Size of arr[0] %d \n",sizeof(arr),sizeof(arr[0]));// 8 4
-    for (int i = 0; i < sz; i++) {
-        *(arr + i) = *(arr + i) * 2;
-        // Or
-        // arr[i] = arr[i] * 2;
+
+/// ### Character `Arrays` & `Pointers`
+void MyPrintFun1(const char *c) // or char c[]
+{
+    c[0] = 'Z';
+    while (*c != '\0') {
+        printf("%c", *c);
+        c++;
     }
+    printf("\n");
 }
+
 int main() {
-    int arr[5] = {1, 2, 3, 4, 5}; //Address 200 204 208 212 216
-    int sz = sizeof(arr) / sizeof(arr[0]);
-    Double(arr, sz);
-    for (int i = 0; i < sz; i++) {
-        printf("%d\n", *(arr + i));
-    }
+    char C[] = "Hello";
+    MyPrintFun1(C);
 }
