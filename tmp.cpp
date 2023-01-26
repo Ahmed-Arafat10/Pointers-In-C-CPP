b5c12a6bd2a557ed267a96fab1e585c95edb2e83
Ahmed_Arafat10 <ahmedxarafat0101@gmail.com>
2023-01-26 14:51:25 +0200

    2:51 pm 1/26/23

--- tmp.cpp
+++ tmp.cpp
@@ -1,19 +1,19 @@
 // Created by Ahmed Arafat on 1/10/2023.
 #include <bits/stdc++.h>
+
 using namespace std;
 
-/// ### Character `Arrays` & `Pointers`
-void MyPrintFun1(const char *c) // or char c[]
-{
-    c[0] = 'Z';
-    while (*c != '\0') {
-        printf("%c", *c);
-        c++;
-    }
-    printf("\n");
-}
-
 int main() {
-    char C[] = "Hello";
-    MyPrintFun1(C);
+    int n;
+    printf("Enter size of the array\n");
+    scanf("%d", &n);
+    int *arr = (int *) malloc(n*sizeof(int));
+    for (int i = 0; i < n; i++) {
+        arr[i] = i+1;
+    }
+    arr = (int*) realloc(arr,n*2*sizeof(int));
+    printf("Address of arr %d , Address of arr2 %d\n",arr,arr2);
+    for (int i = 0; i < n*2; i++) {
+        printf("%d ", arr2[i]);
+    }
 }
